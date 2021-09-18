a1e511d85e74e31033d7ac8bca204aca04389fbd
Exequiel <88289516+ExequielGcu@users.noreply.github.com>
2021-09-18 01:42:48 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -4,6 +4,21 @@
 document.addEventListener("DOMContentLoaded", function(e){
 });
 
+//Aca empieza la abrida y cerrada de la ventana modal
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
+
+//Aca termina la abrida y cerrada de la ventana modal
+
 function usuario(){
 
 
