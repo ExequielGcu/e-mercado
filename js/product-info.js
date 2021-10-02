adbac0c5a97905475030f166a3e41dd8365a80d2
Exequiel <88289516+ExequielGcu@users.noreply.github.com>
2021-10-02 00:15:32 -0300

    Add files via upload

--- js/product-info.js
+++ js/product-info.js
@@ -39,19 +39,79 @@ function traerAuto(car) {
     infocar = `
         <div class="auto">
             <h2>${car.name}</h2>
-            <img src="${car.images[0]}" style="width:150px;"></img>
-            <img src="${car.images[1]}" style="width:150px;"></img>
-            <img src="${car.images[2]}" style="width:150px;"></img>
-            <img src="${car.images[3]}" style="width:150px;"></img>
-            <img src="${car.images[4]}" style="width:150px;"></img><br>
             <strong>${car.description}</strong><br>
             <p>PRECIO EN ${car.currency}: <strong>${car.cost}</strong></p>
+            <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-interval="10000">
+        <img src="${car.images[0]}" class="d-block w-50" alt="...">
+      </div>
+      <div class="carousel-item" data-interval="2000">
+        <img src="${car.images[1]}" class="d-block w-50" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="${car.images[2]}" class="d-block w-50" alt="...">
+      </div>
+      <div class="carousel-item">
+      <img src="${car.images[3]}" class="d-block w-50" alt="...">
+    </div>
+    <div class="carousel-item">
+    <img src="${car.images[4]}" class="d-block w-50" alt="...">
+  </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
         </div>
         <br>
     `;
 
     document.getElementById("productosInformacion").innerHTML = infocar;
+
+    /*Trae los autos relacionados y los carga*/
+    localStorage.setItem("carUno",JSON.stringify(car.relatedProducts));
+    var relacion = localStorage.getItem("carUno");
+    relacion = JSON.parse(relacion);
+    
+    var URLS = [
+        "",
+        "https://exequielgcu.github.io/api-alternativo/product-info-auto-chevrolet-onxi.json",
+        "https://exequielgcu.github.io/api-alternativo/product-info-auto-fiat-way.json",
+        "https://exequielgcu.github.io/api-alternativo/product-info-auto-susuki-celerio.json",
+        "https://exequielgcu.github.io/api-alternativo/product-info-auto-Peugeot-208.json"
+    ]
+
+    for(let i = 0; i < relacion.length; i++){
+        
+        
+        getJSONData(URLS[relacion[i]]).then(function (result) {
+            if (result.status === "ok") {
+                autoArrayRelacionado = result.data;
+
+                infocarRelacion = `
+                    <div class="relacionCar">
+                        <h2 class="h2realcionCar">Auto Relacionado: ${relacion[i]} ${autoArrayRelacionado.name}</h2>
+                        <img src="${autoArrayRelacionado.images[0]}" style="width:200px; margin:10px; border: 5px solid red;"></img>
+                        <strong>${autoArrayRelacionado.description}</strong><br>
+                    </div>
+                `;
+            
+                document.getElementById("autosRelacionados").innerHTML += infocarRelacion;
+                
+            }
+    
+        })
+    }
+     /*Trae el auto realcionado*/ 
 }
+
+
 
 //Llama la funcion encuentraAuto y trae el JSON correspondiente
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -64,6 +124,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
     })
 });
+
 
 
 //Funcion traer comentarios
